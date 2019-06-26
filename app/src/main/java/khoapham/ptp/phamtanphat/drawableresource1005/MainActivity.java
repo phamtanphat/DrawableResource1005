e8d9ecd1bed67b900f0e2ef6e2135cd20296b42a
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-06-26 13:48:05 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/drawableresource1005/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/drawableresource1005/MainActivity.java
@@ -8,39 +8,38 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnTest1,btnTest2,btnTest3;
+//    Button btnTest1,btnTest2,btnTest3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnTest1 = findViewById(R.id.buttonTest1);
-        btnTest2 = findViewById(R.id.buttonTest2);
-        btnTest3 = findViewById(R.id.buttonTest3);
+//        btnTest1 = findViewById(R.id.buttonTest1);
+//        btnTest2 = findViewById(R.id.buttonTest2);
+//        btnTest3 = findViewById(R.id.buttonTest3);
 
-        btnTest1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                btnTest1.setSelected(true);
-                btnTest2.setSelected(false);
-                btnTest3.setSelected(false);
-            }
-        });
-        btnTest2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                btnTest1.setSelected(false);
-                btnTest2.setSelected(true);
-                btnTest3.setSelected(false);
-            }
-        });
-        btnTest3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                btnTest1.setSelected(false);
-                btnTest2.setSelected(false);
-                btnTest3.setSelected(true);
-            }
-        });
-
+//        btnTest1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                btnTest1.setSelected(true);
+//                btnTest2.setSelected(false);
+//                btnTest3.setSelected(false);
+//            }
+//        });
+//        btnTest2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                btnTest1.setSelected(false);
+//                btnTest2.setSelected(true);
+//                btnTest3.setSelected(false);
+//            }
+//        });
+//        btnTest3.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                btnTest1.setSelected(false);
+//                btnTest2.setSelected(false);
+//                btnTest3.setSelected(true);
+//            }
+//        });
     }
 }
